4e55e62e449a6b443b7a57153d1ce2d3823b6ec8
Михаил <mishaistolbikov@gmail.com>
2026-09-05 12:29:52 +0300

    test2

--- test/test_task.py
+++ test/test_task.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from main import app
 from app.db import models
 from app.api.tasks import get_db
+import pytest_asyncio
 
 TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 test_engine = create_async_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -15,13 +16,11 @@ async def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-@pytest.fixture(autouse=True)
+@pytest_asyncio.fixture(autouse=True)
 async def prepare_database():
     async with test_engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.create_all)
-
     yield
-
     async with test_engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.drop_all)
 
